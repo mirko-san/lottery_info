6b4ae6bb618259b3b8d779f51e7b5c84ca0f5dc9
mirko-san <41379069+mirko-san@users.noreply.github.com>
2018-11-12 03:22:58 +0900

    [Fix] async await周辺の改善

--- latest.js
+++ latest.js
@@ -3,9 +3,57 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 module.exports.get_latest = async (event, context) => {
-  let message = await getHTML('https://kageki.hankyu.co.jp/revue/index.html');
-  const date = await get_date_each_title(message);
-  function getHTML(url) {
+  let message = await get_html('https://kageki.hankyu.co.jp/revue/index.html');
+  for (let i = 0; i < message.length; i++) {
+    let content = await get_each_content(message[i].url);
+    if (0 !== Object.keys(content).length) {
+      message[i].first = content.first;
+      message[i].second = content.first;
+      message[i].lottery = true;
+    } else {
+      message[i].lottery = false;
+    }
+  }
+
+  function get_each_content(url) {
+    // TODO:urlからの取得だと公演期間と抽選期間のズレが吸収できない
+    // const year = url.match(/^https:\/\/kageki.hankyu.co.jp\/revue\/([0-9]{4})\//)[1];
+    const year = '2018';
+    const res = {};
+
+    function parse_date(string, year) {
+      const prese = string.replace(/（.*）/, ' ').match(/^([0-9]*?)月([0-9]*?)日\s(.+)$/);
+      function zeropadding(i) {
+        var num = ('00' + i).slice(-2);
+        return num;
+      }
+      return year + '-' + zeropadding(prese[1]) + '-' + zeropadding(prese[2]) + 'T' + prese[3] + ':00+09:00';
+    }
+
+    return new Promise(resolve => {
+      request(url, (e, response, body) => {
+        const $ = cheerio.load(body);
+        $('h3').each(function (i, elem) {
+          if ($(this).text() == '先行販売') {
+            const text = $(this).parent().find('.txt').eq(0).text();
+            const reg_first = /第1抽選方式：(.*)〜(.*)/;
+            const reg_second = /第2抽選方式：(.*)〜(.*)/;
+            res.first = [
+              parse_date(text.match(reg_first)[1], year),
+              parse_date(text.match(reg_first)[2], year)
+            ];
+            res.second = [
+              parse_date(text.match(reg_second)[1], year),
+              parse_date(text.match(reg_second)[2], year)
+            ];
+          }
+        });
+        resolve(res);
+      });
+    });
+  };
+
+  function get_html(url) {
     return new Promise((resolve, reject) => {
       request(url, (e, response, body) => {
         const $ = cheerio.load(body);
@@ -23,52 +71,6 @@ module.exports.get_latest = async (event, context) => {
     });
   }
 
-  async function get_date_each_title(obj) {
-
-    for (let i = 0; i < obj.length; i++) {
-      let date = await get_date(obj[i].url);
-      obj[i].lottery = false;
-      if (date.first) {
-        obj[i].lottery = true;
-        obj[i].first = date.first;
-        obj[i].second = date.second;
-      }
-    }
-
-    function parse_date(string, year) {
-      const prese = string.replace(/（.*）/, ' ').match(/^([0-9]*?)月([0-9]*?)日\s(.+)$/);
-      function zeropadding(i) {
-        var num = ('00' + i).slice(-2);
-        return num;
-      }
-      return year + '-' + zeropadding(prese[1]) + '-' + zeropadding(prese[2]) + 'T' + prese[3] + ':00+09:00';
-    }
-    function get_date(url) {
-      // TODO:urlからの取得だと公演期間と抽選期間のズレが吸収できない
-      // const year = url.match(/^https:\/\/kageki.hankyu.co.jp\/revue\/([0-9]{4})\//)[1];
-      const year = '2018';
-      const res = {};
-      return new Promise((resolve, reject) => {
-        request(url, (e, response, body) => {
-          const $ = cheerio.load(body);
-          $('h3').each(function (i, elem) {
-            if ($(this).text() == '先行販売') {
-              const text = $(this).parent().find('.txt').eq(0).text();
-              const reg_first = /第1抽選方式：(.*)〜(.*)/;
-              const reg_second = /第2抽選方式：(.*)〜(.*)/;
-              const first = [parse_date(text.match(reg_first)[1], year), parse_date(text.match(reg_first)[2], year)];
-              const second = [parse_date(text.match(reg_second)[1], year), parse_date(text.match(reg_second)[2], year)];
-              res.first = first;
-              res.second = second;
-            }
-          });
-          return resolve(res);
-        });
-      });
-    }
-  }
-
-  console.log(message);
   return {
     statusCode: 200,
     body: JSON.stringify({
